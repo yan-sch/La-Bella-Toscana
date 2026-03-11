7d5781d2bed89f6586d94b84bccb3ef3a085bf83
Yannick Schawaller <yannick.schawaller@gmx.de>
2026-03-11 17:08:48 +0100

    Menu categories added

--- speisekarte.js
+++ speisekarte.js
@@ -1,6 +1,7 @@
-const speisekartenDaten = `Titel	Beschreibung	Preis
-Bruschetta	Geröstetes Brot in Olivenöl mit Tomaten & Knoblauch	10,50€
-Pizza Salami	Steinofenpizza Tomatensoße, Mozarella, Tunfisch & Salami Milano	12,50€
-Pizza Tonno	Steinofenpizza Tomatensoße, Mozarella, Tunfisch & Zwiebeln	13,50€
-Spaghetti Napoli	Spaghetti mit Tomatensoße & Olivenöl	9,50€
-Spaghetti Bolognese	Spaghetti mit Bolognese Ragout	13,50€`;
+const speisekartenDaten = `Kategorie	Titel	Beschreibung	Preis
+Vorspeise	Bruschetta	Geröstetes Brot in Olivenöl mit Tomaten & Knoblauch	10,50€
+Pizza	Pizza Salami	Steinofenpizza Tomatensoße, Mozarella, Tunfisch & Salami Milano	12,50€
+Pizza	Pizza Tonno	Steinofenpizza Tomatensoße, Mozarella, Tunfisch & Zwiebeln	13,50€
+Nudeln	Spaghetti Napoli	Spaghetti mit Tomatensoße & Olivenöl	9,50€
+Nudeln	Spaghetti Bolognese	Spaghetti mit Bolognese Ragout	13,50€
+Dessert	Tiramisu	Nach Großmutters Rezept	7,00€`;
